ab14d3a98aaae7900a2698fcb0dafe9689a78779
cansu.akca <cansu.akca@softtech.com.tr>
2026-03-09 13:16:29 +0300

    Refactor CashPayment: add @Override to masked()
    Refactor CashPayment: add @Override to masked()

--- language-refactoring-lab/src/main/java/com/example/model/CashPayment.java
+++ language-refactoring-lab/src/main/java/com/example/model/CashPayment.java
@@ -1,6 +1,13 @@
 package com.example.model;
 
 public class CashPayment extends PaymentMethod {
-    public CashPayment() { super(PaymentMethodType.CASH); }
-    public String masked() { return "CASH"; }
+
+    public CashPayment() {
+        super(PaymentMethodType.CASH);
+    }
+
+    @Override
+    public String masked() {
+        return "CASH";
+    }
 }
